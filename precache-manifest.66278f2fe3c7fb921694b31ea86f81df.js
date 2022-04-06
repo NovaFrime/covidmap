3ebd9d78fe1b651d8fa6e68c0134f134fa8eb8f8
NovaFrost <holyprime1985@gmail.com>
2022-04-06 19:03:39 +0700

    Updates

---
+++ precache-manifest.66278f2fe3c7fb921694b31ea86f81df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765daf4405c4ffe82a27ede046a9b980",
+    "url": "/covidmap/index.html"
+  },
+  {
+    "revision": "5949d79d81f0118bc199",
+    "url": "/covidmap/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "850cefe3f8a3d8d1da11",
+    "url": "/covidmap/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5949d79d81f0118bc199",
+    "url": "/covidmap/static/js/2.4409b690.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covidmap/static/js/2.4409b690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850cefe3f8a3d8d1da11",
+    "url": "/covidmap/static/js/main.bc2703c3.chunk.js"
+  },
+  {
+    "revision": "1cccd29dbbc2ffbccd8c",
+    "url": "/covidmap/static/js/runtime-main.eae4b411.js"
+  }
+]);
